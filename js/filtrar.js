20476310dd050f6a3f04009073d6e7260725e992
bryankk <dantedevilkk@outlook.com>
2019-07-16 16:25:38 -0300

    atualizado
    foi atualizado a barra para pesquisa dos pontos

--- js/filtrar.js
+++ js/filtrar.js
@@ -1,4 +1,4 @@
-var campoFiltro = document.querySelector("#filtrar-tabela");
+var campoFiltro = document.querySelector("#filtrar-nome");
 
 campoFiltro.addEventListener("input", function(){
     console.log(this.value);
@@ -8,6 +8,81 @@ campoFiltro.addEventListener("input", function(){
         for (var i = 0; i < pacientes.length; i++){
             var paciente = pacientes[i];
             var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            if (nome != this.value){
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        }
+    } else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+});
+
+var campoFiltro = document.querySelector("#filtrar-parceria");
+
+campoFiltro.addEventListener("input", function(){
+    console.log(this.value);
+    var pacientes = document.querySelectorAll(".nomes-lista");
+
+    if (this.value.length > 0){
+        for (var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-parceria");
+            var nome = tdNome.textContent;
+            if (nome != this.value){
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        }
+    } else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+});
+
+var campoFiltro = document.querySelector("#filtrar-local");
+
+campoFiltro.addEventListener("input", function(){
+    console.log(this.value);
+    var pacientes = document.querySelectorAll(".nomes-lista");
+
+    if (this.value.length > 0){
+        for (var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-local");
+            var nome = tdNome.textContent;
+            if (nome != this.value){
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        }
+    } else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+});
+
+var campoFiltro = document.querySelector("#filtrar-data");
+
+campoFiltro.addEventListener("input", function(){
+    console.log(this.value);
+    var pacientes = document.querySelectorAll(".nomes-lista");
+
+    if (this.value.length > 0){
+        for (var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-data");
             var nome = tdNome.textContent;
             if (nome != this.value){
                 paciente.classList.add("invisivel");
